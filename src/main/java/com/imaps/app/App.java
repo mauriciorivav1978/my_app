2dda01a0d4b6eb671343c0802be8d8f0986e0847
mauriciorivav1798 <mauricio.rivadeneira@gmail.com>
2022-10-19 22:07:11 -0500

    Proyecto: my-app Desc: Version Inicial del Proyecto

---
+++ src/main/java/com/imaps/app/App.java
@@ -0,0 +1,13 @@
+package com.imaps.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
